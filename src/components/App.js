a10956ffde18836afd00b0e328d46fed3801e6ac
Nurbolat02 <79019914+Nurbolat02@users.noreply.github.com>
2026-09-25 11:19:31 +0200

    update tree

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
 import { useEffect, useState } from 'react';
 import '../styles/App.css';
-function App() {
-  const [data, setData] = useState()
-useEffect(() => {
-	fetch('/data.json')
-		.then((res) => res.json())
-		.then((data)=>setData(data));
-}, []);
 
-console.log(data)
-  return (
-    <div className="App">
-      PIZDA
-    </div>
-  );
+function recursiveTree(data){
+	return Object.entries(data).map(([key, value])=>{
+		if(value.type==='folder'){
+			return <details key={key} >
+				<summary> Folder {key}</summary>
+				{recursiveTree(value.children)}
+			</details>
+		}
+		else{
+			return <div key={key} > File {key} </div>
+		}
+	})
+}
+
+function App() {
+	const [data, setData] = useState();
+
+	useEffect(()=>{
+		fetch('./fs.json').then((res)=>res.json()).then(setData)
+	},[])
+
+
+	if(!data){
+		return <div>Loading...</div>
+	}
+
+	return <div className="App">{recursiveTree(data.root)}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './styles/index.css';
+// import App from './components/App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './components/App';
-
+import AppURL from './components/AppURL';
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<BrowserRouter>
+		<AppURL />
+	</BrowserRouter>
 );
-
-
